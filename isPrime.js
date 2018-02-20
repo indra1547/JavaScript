3dcadcba416653459fc8f08c7afdf275ca7aa909
Indra Alapati <indrababu1547@gmail.com>
2018-02-19 20:30:22 -0600

    Fixed bugs

--- isPrime.js
+++ isPrime.js
@@ -1,14 +1,16 @@
-/* Simple program to check given number is prime or not*/
+/* Simple program to check given number is prime or not with less number of searches*/
 let x;
 
 function isPrime(x) {
   if (x === 2) {
     return true;
   }
+  // check if x is multiple of 2
   if (x % 2 === 0) {
     return false;
   }
-  for (let i = 3; i * i < x; i++) {
+  //else check odd.
+  for (let i = 3; i * i <=x; i+=2) {
     if (x % i == 0) {
       return false;
     }
@@ -16,4 +18,4 @@ function isPrime(x) {
   return true;
 }
 
-console.log(isPrime(7));
+console.log(isPrime(25));
