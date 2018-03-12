f496f8ef85d192986e4d8e4f6ed30611c6adc63d
Indra Alapati <indrababu1547@gmail.com>
2018-03-12 03:29:31 -0500

    some fixes

--- SampleWebPage/js/script.js
+++ SampleWebPage/js/script.js
@@ -1,5 +1,5 @@
-var header = document.querySelector('header');
-var section = document.querySelector('section');
+let header = document.querySelector('header');
+let section = document.querySelector('section');
 
 const obj = {
     "profiles": "Dispalying profiles",
@@ -49,34 +49,34 @@ const obj = {
 }
 /* function to populate header */
 function populateHeader(obj) {
-  var myH1 = document.createElement('h1');
+  let myH1 = document.createElement('h1');
   myH1.textContent = obj.profiles;
   header.appendChild(myH1);
 
 }
 /*Function to show profiles */
 function showProfiles(obj) {
-      var profiles = obj.members;
-      for(var i = 0; i < profiles.length; i++) {
-        var myArticle = document.createElement('article');
-        var myH2 = document.createElement('h2');
-        var myPara1 = document.createElement('p');
-        var myPara2 = document.createElement('p');
-        var myPara3 = document.createElement('p');
-        var myList = document.createElement('ul');
+      let profiles = obj.members;
+      for(let i = 0; i < profiles.length; i++) {
+        let myArticle = document.createElement('article');
+        let myH2 = document.createElement('h2');
+        let myPara1 = document.createElement('p');
+        let myPara2 = document.createElement('p');
+        let myPara3 = document.createElement('p');
+        let myList = document.createElement('ul');
         myH2.textContent = profiles[i].name;
        
         myPara1.textContent = 'Age: ' + profiles[i].age;
         myPara2.textContent = 'Maritial Status:' + profiles[i].isMarried;
         myPara3.textContent = 'Children : ';
-        var child = profiles[i].children;
+        let child = profiles[i].children;
         if(child.length === 0)
         myPara3.style.visibility = "hidden";
-        for(var j = 0; j < child.length; j++) {
-          var listItem = document.createElement('li');
+        for(let j = 0; j < child.length; j++) {
+          let listItem = document.createElement('li');
           listItem.textContent = 'name :'+child[j].name;
           myList.appendChild(listItem);
-          var listItem2 = document.createElement('li')
+          let listItem2 = document.createElement('li')
           listItem2.textContent = 'age :'+child[j].age;
            myList.appendChild(listItem2);
           
